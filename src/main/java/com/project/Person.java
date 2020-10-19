801cf767ec23519981706e5377513b6c473c02ac
Marcy <m0861016@gm.ncue.edu.tw>
2020-10-19 15:17:00 +0800

    Initial commit

---
+++ src/main/java/com/project/Person.java
@@ -0,0 +1,7 @@
+package com.project;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/kotlin/com/project/Hello.kt
@@ -0,0 +1,14 @@
+package com.project
+
+fun main() {
+    //println("Hello kotlin")
+    // Human().hello()
+    val h = Human();
+    h.hello;
+}
+
+class Human {
+    fun hello(){
+        println("Hello KOTLIN")
+    }
+}
